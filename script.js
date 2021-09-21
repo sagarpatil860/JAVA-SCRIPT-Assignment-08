79fa871b161788594bcb2688f2e9edd53062a912
DESKTOP-0HGCM0J\Sagar Patil <sagarpatil860@gmail.com>
2021-09-21 23:13:20 +0530

    JAVA SCRIPT Assignment 11

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("aurl").addEventListener("click", funone);
+function funone(){
+    alert("You have opted for first one");
+}
+
+document.getElementById("aurm").addEventListener("click", funtwo);
+function funtwo(){
+    alert("You have opted for second one");
+}
+
+document.getElementById("aurn").addEventListener("click", funthree);
+function funthree(){
+    alert("You have opted for third one");
+}
+
+document.getElementById("auro").addEventListener("click", funfour);
+function funfour(){
+    alert("You have opted for fourth one");
+}
+
+document.getElementById("aurp").addEventListener("click", funfive);
+function funfive(){
+    alert("You have opted for fifth one");
+}
